1c13491b79c9b098284a14a462bd8ac1c38dd3e7
Juan Semprun <juansemprun.dev@gmail.com>
2024-03-09 22:21:47 +0100

    Solution Exercise 2: live-edtable toast preview

--- src/components/ToastPlayground/ToastPlayground.js
+++ src/components/ToastPlayground/ToastPlayground.js
@@ -1,6 +1,7 @@
 import React, { useState } from 'react';
 
 import Button from '../Button';
+import Toast from '../Toast/Toast';
 
 import styles from './ToastPlayground.module.css';
 
@@ -9,6 +10,11 @@ const VARIANT_OPTIONS = ['notice', 'warning', 'success', 'error'];
 function ToastPlayground() {
   const [message, setMessage] = useState('');
   const [variant, setVariant] = useState('notice');
+  const [isRendered, setIsRendered] = useState(false);
+
+  function handleDismiss() {
+    setIsRendered(false);
+  }
 
   return (
     <div className={styles.wrapper}>
@@ -16,6 +22,12 @@ function ToastPlayground() {
         <img alt="Cute toast mascot" src="/toast.png" />
         <h1>Toast Playground</h1>
       </header>
+
+      {isRendered && (
+        <Toast variant={variant} handleDismiss={handleDismiss}>
+          {message}
+        </Toast>
+      )}
 
       <div className={styles.controlsWrapper}>
         <div className={styles.row}>
@@ -68,7 +80,9 @@ function ToastPlayground() {
           <div
             className={`${styles.inputWrapper} ${styles.radioWrapper}`}
           >
-            <Button>Pop Toast!</Button>
+            <Button onClick={() => setIsRendered(true)}>
+              Pop Toast!
+            </Button>
           </div>
         </div>
       </div>
